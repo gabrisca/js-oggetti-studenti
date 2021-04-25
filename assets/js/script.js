541d78c65aa57e6908c057600cf01f5d14787460
gabrisca <gabrielescarparo1985@gmail.com>
2021-04-25 18:43:42 +0200

    aggiornamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,8 @@ for (var chiave in student) {
 }
 
 // 3 creo un array di oggetti students
-var students = [{
+var students = [
+  {
     firstName: "Harry",
     lastName: "Potter",
     age: 17,
@@ -65,65 +66,71 @@ var students = [{
   },
 ];
 
-// stampo l'array
-console.log(students);
-
 // push di un nuovo studente in fondo all'array (per allenamento)
 students.push({
   firstName: "Michael",
   lastName: "Jordan",
   age: 55,
-})
-
-// 5 Do la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-var newFirstName = prompt("Nome");
-var newLastName = prompt("Cognome");
-var newAge = parseInt(prompt("Età"));
-
-// pusho il tutto nell'array students
-students.push({
-  firstName: newFirstName,
-  lastName: newLastName,
-  age: newAge,
 });
 
-console.log(students);
-
 // 4 Ciclo su tutti gli studenti e stampo per ognuno di essi, nome, cognome ed età
+// ciclo e stampo l'array
+
+// OPTION 1
+// for(var key in students){
+//   var idStudent = students[key];
+//   console.log("Nome: " + idStudent.firstName)
+//   console.log("Cognome: " + idStudent.lastName)
+//   console.log("Età " + idStudent.age)
+//   console.log("----------------------------------------------")
+// }
+
+// OPTION 2
+// ciclo l'array con FOR OF per farmi restituire tutti i contenuti dell'oggetto
 for (var idStudent of students) {
-  // vedo tutti gli studenti dell'array
-  console.log(idStudent);
-  // ciclo for-in e stampo tutti gli studenti con le realtive prorpietà
-  for (var chiave in idStudent) {
-    // vedo le chiavi
-    console.log(chiave);
-    // vedo i valori
-    console.log(idStudent[chiave]);
-    // vedo le chiavi e i relativi valori
-    console.log("chiave: " + chiave + " - valore: " + idStudent[chiave]);
-  }
-  console.log("-------------");
+  console.log("Nome: " + idStudent.firstName);
+  console.log("Cognome: " + idStudent.lastName);
+  console.log("Età " + idStudent.age);
+  console.log("----------------------------------------------");
 
-
-  // ALLENAMENTO //
-
-  //salvo in var le ul creatE in html
+  // ALLENAMENTO 1 //
+  //salvo in var le ul create in html
   var ulFirstName = document.getElementById("nome");
   var ulLastName = document.getElementById("cognome");
   var ulAge = document.getElementById("età");
 
   // salvo in var le stringhe html con gli elementi dell'array
   var strHtmlFirstName =
-    "<li><p>" + idStudent.firstName + '</p>';
-  var strHtmlLastName =
-    "<li><p>" + idStudent.lastName + '</p>';
-  var strHtmlAge =
-    "<li><p>" + idStudent.age + '</p>';
+    "<li><p class= 'target' >" + idStudent.firstName + "</p>";
+  var strHtmlLastName = "<li><p class= 'target'>" + idStudent.lastName + "</p>";
+  var strHtmlAge = "<li><p class= 'target'>" + idStudent.age + "</p>";
 
   // aggiungo i contenuti delle stringhe prima della chiusra dell'ul
   ulFirstName.innerHTML += strHtmlFirstName;
   ulLastName.innerHTML += strHtmlLastName;
   ulAge.innerHTML += strHtmlAge;
-  
   // giunto qui visualizzo sul browser il contenuto dell'array suddiviso in ul
+}
+
+// ALLENAMENTO 2 //
+// cerco se uno studente è presente nell'elenco
+// ciclo FOR IN che mi restituisce le chiavi con relativi valori
+for (var key in students) {
+  var idStudent = students[key];
+  console.log("Nome: " + idStudent.firstName);
+  console.log("Cognome: " + idStudent.lastName);
+  console.log("Età " + idStudent.age);
+  console.log("----------------------------------------------");
+
+  //  salvo in una var l'id search
+  var search = document.getElementById("search");
+
+  // salvo un delle variabili i nomi e i cognomi degli studenti
+  var searchName = idStudent.firstName;
+  var searchSurname = idStudent.lastName;
+
+  // se il valore inserito nell'input è uguale a uno dei valori di idstudente...
+  if (search.value === searchName || search.value === searchSurname) {
+    alert("Lo studente " + search.value + " è presente nell'elenco");
+  }
 }
